250b402a07d6b6295442fc2be8cb78610e38b37b
sureshdvp <suresh.devjava@gmail.com>
2019-07-25 20:09:14 +0530

    Update GitHubJsonEntity.java

--- src/main/java/com/coditas/reposervice/entity/github/model/GitHubJsonEntity.java
+++ src/main/java/com/coditas/reposervice/entity/github/model/GitHubJsonEntity.java
@@ -3,15 +3,6 @@ package com.coditas.reposervice.entity.github.model;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
-
-@Getter
-@Setter
-@NoArgsConstructor
-@ToString
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class GitHubJsonEntity {
 	private int id;
